5ff6366ab10cd1c7e9334a8cfbc379481aec6167
Carlos Daniel Martínez López <carlos.martinez.cbtis59@gmail.com>
2023-02-17 12:23:36 -0600

    nueva interfaz con bd funcional

---
+++ php/db_connection.php
@@ -0,0 +1,13 @@
+<?php
+$serverName = "DESKTOP-GBQUMCE";
+$connectionInfo = array("Database"=>"Sistema_Inventario", "UID"=>"sa", "PWD"=>"12345");
+
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+if ($conn) {
+  echo "Conexión establecida.";
+} else {
+  echo "Conexión no se pudo establecer.";
+  die(print_r(sqlsrv_errors(), true));
+}
+?>
---
+++ php/loginController.php
@@ -0,0 +1,22 @@
+<?php
+    require_once 'db_connection.php';
+
+    $tsql = "SELECT * FROM tblLogin WHERE Usuario = ? AND Contrasena = ?";
+    $params = array($_POST['usuario'], $_POST['contraseña']);
+
+    $stmt = sqlsrv_query($conn, $tsql, $params);
+
+    if ($stmt === false) {
+        echo "Error en la consulta.";
+        die(print_r(sqlsrv_errors(), true));
+    }
+
+    if (sqlsrv_has_rows($stmt)) {
+        header("Location: ../home.html");
+    }else {
+        echo "Usuario o contraseña incorrectos";
+    }
+    
+    sqlsrv_free_stmt($stmt);
+    sqlsrv_close($conn);
+?>
